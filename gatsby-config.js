e066453681c0ad6800ea8b80f260e24149e7bac9
System Administrator <root@Sahils-MacBook-Air.local>
2025-01-17 20:29:55 +0530

    changes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,14 +12,14 @@ module.exports = {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
-      "path": "./src/images/"
+      "path": "src/images/"
     },
     __key: "images"
   }, {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
-      "path": "./src/_posts/blog"
+      "path": "src/_posts/blog"
     },
     __key: "pages"
   },
